e9d53b293358877bad22c09500c735746673d5fc
Ruben97-github <san.ruben97@gmail.com>
2017-06-18 11:28:06 +0530

    Add files via upload

---
+++ C++ Projects/Project 1/main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int choice;
+float value1, value2, result;
+
+int main()
+{
+    cout << "CALCULATOR\nBy Ruben Santos\n\n";
+
+    while(true)
+    {
+        cout << "1. Add\n" <<
+                "2. Subtract\n" <<
+                "3. Divide\n" <<
+                "4. Multiply\n" <<
+                "5. Exit\n\n" <<
+                "What would you like to do? ";
+
+             cin >> choice;
+
+             if(choice == 5)
+                break;
+
+            cout << "\nPlease enter the first value: ";
+            cin >> value1;
+            cout << "Please enter the second value: ";
+            cin >> value2;
+           if(choice == 1)
+            result = value1 + value2;
+           else if(choice == 2)
+            result = value1 - value2;
+           else if(choice == 3)
+            result = value1 / value2;
+           else if(choice == 4)
+            result = value1 * value2;
+
+            cout << "\nThe result is: " << result << "\n\n";
+
+    }
+ cout << "\n\nGood Bye!\n";
+}
